cf3c7791b4ebd71b548af8f9ec13cd78fd9587e4
aethagoan <aethagoan@outlook.com>
2025-02-25 17:41:18 -0700

    added a thing that kinda works?

--- GameLogic/GameLoopRunner.cs
+++ GameLogic/GameLoopRunner.cs
@@ -2,50 +2,110 @@ namespace GameLogic.Game;
 
 public class GameLoopRunner
 {
-  private readonly Game game;
-  private object loopLock { get; } = new object();
-  private bool loopIsRunning { get; set; } = false;
-  public static double TickIntervalScalar = 10;
+    private readonly Game game;
+    private object loopLock { get; } = new object();
+    private object fileLock { get; } = new object();
+    private bool loopIsRunning { get; set; } = false;
+    public static double TickIntervalScalar = 10;
 
-  public GameLoopRunner(Game game)
-  {
-    this.game = game;
-  }
+    private int tickcounter = 0;
 
+    IEnumerable<Tank> lastTankState;
 
-  public void RunGameLoop()
-  {
-    Task.Run(async () =>
+    public GameLoopRunner(Game game)
     {
-      game.CancellationTokenSource.Token.ThrowIfCancellationRequested();
-      lock (loopLock)
-      {
-        if (loopIsRunning)
+        this.game = game;
+    }
+
+
+    public void RunGameLoop()
+    {
+        Task.Run(async () =>
         {
-          Console.WriteLine("Another thread is already running the loop.");
-          return;
-        }
+            game.CancellationTokenSource.Token.ThrowIfCancellationRequested();
+            lock (loopLock)
+            {
+                if (loopIsRunning)
+                {
+                    Console.WriteLine("Another thread is already running the loop.");
+                    return;
+                }
 
-        loopIsRunning = true;
-      }
+                loopIsRunning = true;
+            }
 
-      while (!game.CancellationTokenSource.Token.IsCancellationRequested)
-      {
-        await ProcessGameTick();
-        var interval = (int)(10 * TickIntervalScalar);
-        // Console.WriteLine($"sleeping {interval}");
-        
-        Thread.Sleep(interval);
-      }
-      loopIsRunning = false;
-      
-    });
-  }
-  public async Task ProcessGameTick()
-  {
-    Console.WriteLine("processing game tick");
+            while (!game.CancellationTokenSource.Token.IsCancellationRequested)
+            {
+                await ProcessGameTick();
+                tickcounter++;
+                var interval = (int)(10 * TickIntervalScalar);
+                // Console.WriteLine($"sleeping {interval}");
 
-    game.Tanks = game.Tanks.Select(Tank.ProcessTankMovement).ToArray();
-    await game.BroadcastUpdate();
-  }
+                Thread.Sleep(interval);
+            }
+            loopIsRunning = false;
+
+        });
+    }
+    public async Task ProcessGameTick()
+    {
+        Console.WriteLine($"processing game tick {tickcounter}");
+
+        //var copy = game.Tanks.ToArray();
+        //foreach (var tank in copy) {
+        //    Console.WriteLine(tank);
+        //}
+        //foreach (var tank in game.Tanks) {
+        //    Console.WriteLine(tank);
+        //}
+        //Console.WriteLine();
+        game.Tanks = game.Tanks.Select(Tank.ProcessTankMovement).ToArray();
+
+
+        _ = Task.Run(() =>
+        {
+            if (game.Tanks != lastTankState)
+            {
+
+
+                // put the append function here.
+                // append a line with the array
+
+                string ticklist = "{";
+                ticklist += $"{tickcounter}, ";
+
+                foreach (var thing in game.Tanks)
+                {
+                    ticklist += thing.ToString();
+                    ticklist += ",";
+                }
+
+                ticklist.Remove(ticklist.Length - 4, 4); // remove the last ', '
+                ticklist += "},\n";
+
+                //Console.WriteLine(ticklist);
+
+                // now I just need to output it to a game location.
+                _ = Task.Run(() =>
+                {
+
+                    if (!File.Exists($"{game.Name}.txt"))
+                    {
+                         File.Create($"{game.Name}.txt").Close();
+
+                        
+                    }
+
+                    lock (fileLock)
+                    {
+                        File.AppendAllText($"{game.Name}.txt", ticklist);
+                    }
+                });
+
+                lastTankState = game.Tanks;
+            }
+        });
+
+        await game.BroadcastUpdate();
+    }
 }
