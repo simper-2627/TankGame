e12d224b901d42975d3047649af680fd87c569e6
aethagoan <aethagoan@outlook.com>
2025-02-26 14:36:56 -0700

    shifted replay saving into its own class

--- GameLogic/GameLoopRunner.cs
+++ GameLogic/GameLoopRunner.cs
@@ -4,14 +4,12 @@ public class GameLoopRunner
 {
     private readonly Game game;
     private object loopLock { get; } = new object();
-    private object fileLock { get; } = new object();
     private bool loopIsRunning { get; set; } = false;
     public static double TickIntervalScalar = 10;
 
     private int tickcounter = 0;
 
-    IEnumerable<Tank> lastTankState;
-
+    private ReplaySaver saver;
     public GameLoopRunner(Game game)
     {
         this.game = game;
@@ -61,48 +59,7 @@ public class GameLoopRunner
         //Console.WriteLine();
         game.Tanks = game.Tanks.Select(Tank.ProcessTankMovement).ToArray();
 
-
-        _ = Task.Run(() =>
-        {
-            if (game.Tanks != lastTankState)
-            {
-
-
-                // put the append function here.
-                // append a line with the array
-
-                string ticklist = "{";
-                ticklist += $"{tickcounter}, ";
-
-                foreach (var thing in game.Tanks)
-                {
-                    ticklist += thing.ToString();
-                    ticklist += ",";
-                }
-
-                ticklist.Remove(ticklist.Length - 4, 4); // remove the last ', '
-                ticklist += "},\n";
-
-                //Console.WriteLine(ticklist);
-
-                // now I just need to output it to a game location.
-                _ = Task.Run(() =>
-                {
-
-                    if (!File.Exists($"{game.Name}.txt"))
-                    {
-                         File.Create($"{game.Name}.txt").Close();
-                    }
-
-                    lock (fileLock)
-                    {
-                        File.AppendAllText($"{game.Name}.txt", ticklist);
-                    }
-                });
-
-                lastTankState = game.Tanks;
-            }
-        });
+        saver.SaveTick(game.Tanks, tickcounter, game.Name);
 
         await game.BroadcastUpdate();
     }
---
+++ GameTest/GameReplayTests.cs
@@ -0,0 +1,19 @@
+﻿using GameLogic;
+using GameLogic.Game;
+using Microsoft.AspNetCore.SignalR;
+
+namespace GameTest
+{
+    internal class GameReplayTests
+    {
+        IEnumerable<Tank> Tanks = new List<Tank>();
+
+        //public Game(IHubContext<LobbyHub> context)
+        //{
+        //    loopRunner = new(this);
+        //    hubContext = context;
+        //}
+
+
+    }
+}
